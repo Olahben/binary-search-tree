ae7f68924275830f317012f403860e1a5e23fe74
Olahben <ohagenbendixen@gmail.com>
2023-07-16 11:51:58 +0100

    Implement mergeSort function inside buildTree

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ function node(data, left, right) {
 }
 
 function buildTree(arr) {
-  function mergeSort(l, h) {
-    const mid = (l + h) / 2;
-  }
-
   function merge(a, b, m, n) {
     const sorted = [];
     let i,
@@ -27,5 +23,16 @@ function buildTree(arr) {
     for (; j <= n; j++) {
       sorted[k++] = b[j];
     }
+  }
+  function mergeSort(arr2) {
+    if (arr2 <= 1) return;
+
+    const l = 0;
+    const h = arr2.length - 1;
+    const mid = (l + h) / 2;
+
+    const left = mergeSort(arr2.slice(l, mid));
+    const right = mergeSort(arr2.slice(mid + 1, h));
+    return merge(left, right, left.length + 1, right.length + 1);
   }
 }
