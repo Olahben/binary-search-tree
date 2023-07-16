77548f31e7e365bbfb6cac098b1e1e1f57b23e1f
Olahben <ohagenbendixen@gmail.com>
2023-07-16 11:29:35 +0100

    Implement the merging part for the mergeSort function

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 function node(data, left, right) {
   return { data, left, right };
 }
+
+function buildTree(arr) {
+  function mergeSort(l, h) {
+    const mid = (l + h) / 2;
+  }
+
+  function merge(a, b, m, n) {
+    const sorted = [];
+    let i,
+      j,
+      k = 1;
+
+    while (i <= m && j <= n) {
+      if (a[i] < b[j]) {
+        sorted[k++] = a[i++];
+      } else {
+        sorted[k++] = b[j++];
+      }
+    }
+
+    for (; i <= m; i++) {
+      sorted[k++] = a[i];
+    }
+    for (; j <= n; j++) {
+      sorted[k++] = b[j];
+    }
+  }
+}
