b9ce8cc1cfb4a93af1f90ece71210daafc483df2
Olahben <ohagenbendixen@gmail.com>
2023-07-18 10:49:47 +0100

    Make the code structured and easier to read

--- app.js
+++ app.js
@@ -2,42 +2,44 @@ function node(data, left, right) {
   return { data, left, right };
 }
 
+function merge(a, b, m, n) {
+  const sorted = [];
+  let i = 0,
+    j = 0,
+    k = 0;
+
+  while (i <= m && j <= n) {
+    if (a[i] <= b[j]) {
+      sorted[k++] = a[i++];
+    } else {
+      sorted[k++] = b[j++];
+    }
+  }
+
+  for (; i <= m; i++) {
+    sorted[k++] = a[i];
+  }
+  for (; j <= n; j++) {
+    sorted[k++] = b[j];
+  }
+
+  return sorted;
+}
+
+function mergeSort(arr2) {
+  const copy = arr2;
+  if (copy.length <= 1) return copy;
+
+  const mid = copy.length / 2;
+
+  const left = mergeSort(copy.slice(0, mid));
+  const right = mergeSort(copy.slice(mid, copy.length));
+  if (left === undefined) return copy;
+  if (right === undefined) return copy;
+  return merge(left, right, left.length - 1, right.length - 1);
+}
+
 function buildTree(arr) {
-  function merge(a, b, m, n) {
-    const sorted = [];
-    let i = 0,
-      j = 0,
-      k = 0;
-
-    while (i <= m && j <= n) {
-      if (a[i] <= b[j]) {
-        sorted[k++] = a[i++];
-      } else {
-        sorted[k++] = b[j++];
-      }
-    }
-
-    for (; i <= m; i++) {
-      sorted[k++] = a[i];
-    }
-    for (; j <= n; j++) {
-      sorted[k++] = b[j];
-    }
-
-    return sorted;
-  }
-  function mergeSort(arr2) {
-    const copy = arr2;
-    if (copy.length <= 1) return copy;
-
-    const mid = copy.length / 2;
-
-    const left = mergeSort(copy.slice(0, mid));
-    const right = mergeSort(copy.slice(mid, copy.length));
-    if (left === undefined) return copy;
-    if (right === undefined) return copy;
-    return merge(left, right, left.length - 1, right.length - 1);
-  }
   const result = mergeSort(arr);
   const set = new Set([]);
   result.forEach((val) => {
@@ -45,6 +47,20 @@ function buildTree(arr) {
   });
   const nonDuplicateResult = [...set]; // An array created with the spread operator
   console.log(nonDuplicateResult);
+
+  function createTree(arr3) {
+    /* const copy = arr3;
+    const start = 0;
+    const end = copy.length - 1;
+    const mid = (start + (end - start)) / 2;
+    const leftSubarray = copy.slice(start, mid);
+    const rightSubarray = copy.slice(mid + 1, end);
+    console.log(leftSubarray);
+    console.log(rightSubarray);
+
+    const root = node(copy[mid]); */
+  }
+  createTree(nonDuplicateResult);
 }
 
 // console.log(buildTree([2, 1, 6, 4, 8, 7, 3, 5]));
